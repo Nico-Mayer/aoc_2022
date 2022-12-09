c1ca28642cb2db7c06406ddeacd9749f2b87c015
Nico-Mayer <nico.m94@icloud.com>
2022-12-09 09:06:03 +0100

    day9

---
+++ src/day9/day9p2.js
@@ -0,0 +1,70 @@
+const { sign } = require('crypto')
+const { getInput } = require('../Utils.js')
+
+const input = getInput({ day: 9, sample: false })
+let steps = input.map((line) => {
+	return line.split(' ')
+})
+let visited = new Set(['0,0'])
+
+let knots = [
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+	[0, 0],
+]
+
+function touching(x1, y1, x2, y2) {
+	return Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1
+}
+
+function moveHead(direction) {
+	switch (direction) {
+		case 'U':
+			knots[0][1]++
+			break
+		case 'R':
+			break
+		case 'D':
+			hy--
+			break
+		case 'L':
+			hx--
+			break
+	}
+
+	if (!touching(hx, hy, tx, ty)) {
+		let sign_x
+		let sign_y = 0
+
+		if (hx == tx) {
+			sign_x = 0
+		} else {
+			sign_x = (hx - tx) / Math.abs(hx - tx)
+		}
+
+		if (hy == ty) {
+			sign_y = 0
+		} else {
+			sign_y = (hy - ty) / Math.abs(hy - ty)
+		}
+
+		tx += sign_x
+		ty += sign_y
+		visited.add(`${tx},${ty}`)
+	}
+}
+
+steps.map((step) => {
+	let [direction, distance] = step
+	for (let i = 0; i < distance; i++) {
+		moveHead(direction)
+	}
+})
+
+console.log(visited.size)
