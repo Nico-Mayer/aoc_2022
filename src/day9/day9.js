586d11a45657cefb88a181b29b5582b8cea1d3cd
Nico-Mayer <nico.m94@icloud.com>
2022-12-09 18:30:55 +0100

    Added Task 7,8,9

--- src/day9/day9.js
+++ src/day9/day9.js
@@ -11,7 +11,18 @@ class Point {
 	}
 }
 
-let rope = [new Point(0, 0), new Point(0, 0)]
+let rope = [
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+	new Point(0, 0),
+]
 let visited = new Set([JSON.stringify(rope[0])])
 
 steps.map((step) => {
@@ -31,19 +42,53 @@ function touching(pointA, pointB) {
 function move(direction) {
 	let head = rope[0]
 	let tail = rope[1]
-	let [prevX, prevY] = [head.x, head.y]
 
 	if (direction === 'U') head.y++
 	else if (direction === 'D') head.y--
 	else if (direction === 'L') head.x--
 	else if (direction === 'R') head.x++
 
-	if (!touching(head, tail)) {
-		tail.x = prevX
-		tail.y = prevY
+	for (let i = 1; i < rope.length; i++) {
+		let head = rope[i - 1]
+		let tail = rope[i]
+		follow(head, tail)
 	}
 
 	visited.add(JSON.stringify(rope[rope.length - 1]))
+}
+
+function follow(head, tail) {
+	let inSameCol = head.x === tail.x
+	let inSameRow = head.y === tail.y
+	if (touching(head, tail)) return
+
+	if (inSameCol) {
+		if (head.y > tail.y) {
+			tail.y++
+		} else {
+			tail.y--
+		}
+	} else if (inSameRow) {
+		if (head.x > tail.x) {
+			tail.x++
+		} else {
+			tail.x--
+		}
+	} else {
+		if (head.x < tail.x && head.y < tail.y) {
+			tail.x--
+			tail.y--
+		} else if (head.x > tail.x && head.y < tail.y) {
+			tail.x++
+			tail.y--
+		} else if (head.y > tail.y && head.x < tail.x) {
+			tail.y++
+			tail.x--
+		} else if (head.y > tail.y && head.x > tail.x) {
+			tail.y++
+			tail.x++
+		}
+	}
 }
 
 console.log(visited.size)
