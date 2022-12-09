7f294c89917fd08f2ce8dfdb3e2a62babc66002d
Nico-Mayer <nico.m94@icloud.com>
2022-12-09 19:07:29 +0100

    short day 6 solution

--- src/day6/day6.js
+++ src/day6/day6.js
@@ -1,32 +1,21 @@
 const { getInput, stringHasDuplicates } = require('../Utils.js')
 
 const input = getInput({ day: 6, sample: false })[0]
-let solution = {
-  p1: { index: 0, found: false },
-  p2: { index: 0, found: false },
-}
+let solution = { i: 0, found: false }
 
+let bufferLength = 14
 let buffer = ''
-let messageBuffer = ''
+
 for (let i = 0; i < input.length; i++) {
-  buffer += input[i]
-  messageBuffer += input[i]
-  if (buffer.length > 4) {
-    buffer = buffer.substring(1)
-  }
-  if (buffer.length === 4 && !solution.p1.found) {
-    if (!stringHasDuplicates(buffer)) {
-      solution.p1 = { index: i + 1, found: true }
-    }
-  }
-  if (messageBuffer.length > 14) {
-    messageBuffer = messageBuffer.substring(1)
-  }
-  if (messageBuffer.length === 14 && !solution.p2.found) {
-    if (!stringHasDuplicates(messageBuffer)) {
-      solution.p2 = { index: i + 1, found: true }
-    }
-  }
+	buffer += input[i]
+	if (buffer.length > bufferLength) {
+		buffer = buffer.substring(1)
+	}
+	if (buffer.length === bufferLength && !solution.found) {
+		if (!stringHasDuplicates(buffer)) {
+			solution = { i: i + 1, found: true }
+		}
+	}
 }
 
 console.log(solution)
