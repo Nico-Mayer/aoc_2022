e6af18fedef3092c59638e060256d85a98faf253
Nico-Mayer <nico.m94@icloud.com>
2022-12-09 10:28:13 +0100

    cleanup day9

--- src/day9/day9.js
+++ src/day9/day9.js
@@ -1,62 +1,41 @@
-const { sign } = require('crypto')
 const { getInput } = require('../Utils.js')
 
 const input = getInput({ day: 9, sample: false })
 let steps = input.map((line) => {
 	return line.split(' ')
 })
-let visited = new Set(['0,0'])
 
 let [hx, hy] = [0, 0]
 let [tx, ty] = [0, 0]
-
-function touching(x1, y1, x2, y2) {
-	return Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1
-}
-
-function moveHead(direction) {
-	switch (direction) {
-		case 'U':
-			hy++
-			break
-		case 'R':
-			hx++
-			break
-		case 'D':
-			hy--
-			break
-		case 'L':
-			hx--
-			break
-	}
-
-	if (!touching(hx, hy, tx, ty)) {
-		let sign_x
-		let sign_y = 0
-
-		if (hx == tx) {
-			sign_x = 0
-		} else {
-			sign_x = (hx - tx) / Math.abs(hx - tx)
-		}
-
-		if (hy == ty) {
-			sign_y = 0
-		} else {
-			sign_y = (hy - ty) / Math.abs(hy - ty)
-		}
-
-		tx += sign_x
-		ty += sign_y
-		visited.add(`${tx},${ty}`)
-	}
-}
+let visited = new Set(['0,0'])
 
 steps.map((step) => {
 	let [direction, distance] = step
 	for (let i = 0; i < distance; i++) {
-		moveHead(direction)
+		move(direction)
 	}
 })
+
+function touching(x1, y1, x2, y2) {
+	let xDiff = Math.abs(x1 - x2)
+	let yDiff = Math.abs(y1 - y2)
+	if (yDiff > 1 || xDiff > 1) return false
+	return true
+}
+
+function move(direction) {
+	let [prevX, prevY] = [hx, hy]
+
+	if (direction === 'U') hy++
+	else if (direction === 'D') hy--
+	else if (direction === 'L') hx--
+	else if (direction === 'R') hx++
+
+	if (!touching(hx, hy, tx, ty)) {
+		tx = prevX
+		ty = prevY
+		visited.add(`${tx},${ty}`)
+	}
+}
 
 console.log(visited.size)
