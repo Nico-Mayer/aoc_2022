0668ced0850ea40b21fde81abc236a9aa70a900d
Nico-Mayer <nico.m94@icloud.com>
2022-12-10 14:55:18 +0100

    day 10 p2 solution

--- src/Utils.js
+++ src/Utils.js
@@ -1,94 +1,100 @@
 const { readFileSync } = require('fs')
 
 function toIntArray(array) {
-  return array.map((item) => parseInt(item))
+	return array.map((item) => parseInt(item))
 }
 
 function sliceIntoChunks(array, chunkSize) {
-  let chunks = []
-  for (let i = 0; i < array.length; i += chunkSize) {
-    chunks.push(array.slice(i, i + chunkSize))
-  }
-  return chunks
+	let chunks = []
+	for (let i = 0; i < array.length; i += chunkSize) {
+		chunks.push(array.slice(i, i + chunkSize))
+	}
+	return chunks
 }
 
 function sortIntArray(array, order = 'asc') {
-  if (order === 'asc') {
-    return array.sort((a, b) => a - b)
-  } else if (order === 'desc') {
-    return array.sort((a, b) => b - a)
-  }
+	if (order === 'asc') {
+		return array.sort((a, b) => a - b)
+	} else if (order === 'desc') {
+		return array.sort((a, b) => b - a)
+	}
 }
 
 function sortStringArrayByLength(array, order = 'asc') {
-  if (order === 'asc') {
-    return array.sort((a, b) => a.length - b.length)
-  } else if (order === 'desc') {
-    return array.sort((a, b) => b.length - a.length)
-  }
+	if (order === 'asc') {
+		return array.sort((a, b) => a.length - b.length)
+	} else if (order === 'desc') {
+		return array.sort((a, b) => b.length - a.length)
+	}
 }
 
 function sortObjectArray(array, key, order = 'asc') {
-  if (order === 'asc') {
-    return array.sort((a, b) => a[key] - b[key])
-  } else if (order === 'desc') {
-    return array.sort((a, b) => b[key] - a[key])
-  }
+	if (order === 'asc') {
+		return array.sort((a, b) => a[key] - b[key])
+	} else if (order === 'desc') {
+		return array.sort((a, b) => b[key] - a[key])
+	}
 }
 
 function findCommonChars(str1, str2, str3 = '') {
-  let commonChar = []
-  for (let i = 0; i < str1.length; i++) {
-    let char = str1[i]
-    if (str2.includes(char) && (str3 ? str3.includes(char) : true)) {
-      commonChar = [...commonChar, char]
-    }
-  }
-  return commonChar
+	let commonChar = []
+	for (let i = 0; i < str1.length; i++) {
+		let char = str1[i]
+		if (str2.includes(char) && (str3 ? str3.includes(char) : true)) {
+			commonChar = [...commonChar, char]
+		}
+	}
+	return commonChar
 }
 
 function containsAll(arr1, arr2) {
-  return arr2.every((item) => arr1.includes(item))
+	return arr2.every((item) => arr1.includes(item))
 }
 
 function containsSingle(arr1, arr2) {
-  return arr2.some((item) => arr1.includes(item))
+	return arr2.some((item) => arr1.includes(item))
 }
 
 function reverseString(str) {
-  return str.split('').reverse().join('')
+	return str.split('').reverse().join('')
 }
 
 function stringHasDuplicates(string) {
-  let hasDuplicate = string.split('').length !== new Set(string.split('')).size
-  return hasDuplicate ? true : false
+	let hasDuplicate =
+		string.split('').length !== new Set(string.split('')).size
+	return hasDuplicate ? true : false
+}
+
+function replaceAt(string, index, replace) {
+	return string.substring(0, index) + replace + string.substring(index + 1)
 }
 
 function getInput({ day, sample }) {
-  const path = sample ? 'sample' : 'input'
-  let data
-  console.log(`Loading data for day ${day} Type: ${path}`)
-  console.log()
-  try {
-    data = readFileSync(`../resources/day${day}/${path}.txt`, 'utf8')
-  } catch (err) {
-    //console.log('Loaded from Source')
-    data = readFileSync(`../../resources/day${day}/${path}.txt`, 'utf8')
-  }
+	const path = sample ? 'sample' : 'input'
+	let data
+	console.log(`Loading data for day ${day} Type: ${path}`)
+	console.log()
+	try {
+		data = readFileSync(`../resources/day${day}/${path}.txt`, 'utf8')
+	} catch (err) {
+		//console.log('Loaded from Source')
+		data = readFileSync(`../../resources/day${day}/${path}.txt`, 'utf8')
+	}
 
-  return data.split(/\r?\n/)
+	return data.split(/\r?\n/)
 }
 
 module.exports = {
-  toIntArray,
-  sortIntArray,
-  sortObjectArray,
-  findCommonChars,
-  sortStringArrayByLength,
-  sliceIntoChunks,
-  containsAll,
-  containsSingle,
-  getInput,
-  reverseString,
-  stringHasDuplicates,
+	toIntArray,
+	sortIntArray,
+	sortObjectArray,
+	findCommonChars,
+	sortStringArrayByLength,
+	sliceIntoChunks,
+	containsAll,
+	containsSingle,
+	getInput,
+	reverseString,
+	stringHasDuplicates,
+	replaceAt,
 }
--- src/day10/day10p2.js
+++ src/day10/day10p2.js
@@ -1,12 +1,51 @@
-const { getInput } = require('../Utils.js')
-const input = getInput({ day: 10, sample: true })
+const { getInput, replaceAt } = require('../Utils.js')
+const input = getInput({ day: 10, sample: false })
 
 let X = 1
 let cycle = 0
+
 let CRT = Array(6).fill('........................................')
 
 for (let line of input) {
-	console.log(line)
+	let op = line.split(' ')
+
+	if (op[0] === 'noop') {
+		updateCRT(X, cycle)
+		cycle += 1
+	} else if (op[0] === 'addx') {
+		let value = parseInt(op[1])
+		updateCRT(X, cycle)
+		cycle += 1
+
+		updateCRT(X, cycle)
+		cycle += 1
+
+		X += value
+	}
+}
+
+function updateCRT(spriteIndex, cycle) {
+	let row
+
+	if (cycle < 40) row = 0
+	else if (cycle < 80) row = 1
+	else if (cycle < 120) row = 2
+	else if (cycle < 160) row = 3
+	else if (cycle < 200) row = 4
+	else if (cycle < 240) row = 5
+
+	let pixelIndex = cycle % 40
+	let sprite = [spriteIndex - 1, spriteIndex, spriteIndex + 1]
+
+	console.log(
+		'Row: ' + row + ' PixelIndex: ' + pixelIndex + ' Sprite: ' + sprite
+	)
+
+	if (sprite.includes(pixelIndex)) {
+		console.log('Print Pixel At: ' + pixelIndex)
+		CRT[row] = replaceAt(CRT[row], pixelIndex, '#')
+	}
+	console.log(' ')
 }
 
 console.table(CRT)
