864b4c5e2a60c4295ed91a025a893dee6d710206
Nico-Mayer <nico.m94@icloud.com>
2022-12-12 01:31:40 +0100

    day 11 cleanup

--- src/day11/day11.js
+++ src/day11/day11.js
@@ -1,93 +1,79 @@
 const { getInput, sliceIntoChunks, split, toIntArray } = require('../Utils.js')
 const input = getInput({ day: 11, sample: false })
 
-let monkeyTexts = sliceIntoChunks(input, 7).map((chunk) => {
-	return chunk.slice(0, 6)
-})
-
-let rounds = 10000
-
+let rounds = 20
 let monkeys = []
+let superModulo = 1
+let p2 = false
 
 class Monkey {
-	constructor(text) {
-		this.id = text[0][7]
-		this.items = toIntArray(text[1].replace(/\s/g, '').slice(14).split(','))
-		this.operation = text[2].replace(/\s/g, '').slice(17)
-		this.test = parseInt(text[3].replace(/\s/g, '').slice(16))
-		this.monkeyIfTrue = parseInt(text[4].replace(/\s/g, '').slice(-1))
-		this.monkeyIfFalse = parseInt(text[5].replace(/\s/g, '').slice(-1))
+	constructor(dna) {
+		this.items = toIntArray(dna[1].slice(14).split(','))
+		this.operation = dna[2].slice(17)
+		this.test = parseInt(dna[3].slice(16))
+		this.monkeyIfTrue = parseInt(dna[4].slice(-1))
+		this.monkeyIfFalse = parseInt(dna[5].slice(-1))
+		this.inspectedItems = 0
 	}
-
-	inspectedItems = 0
 
 	inspect() {
-		console.log('Execute Inspection')
 		let item = this.items[0]
 		let [operator, value] = split(this.operation, 1)
-		if (value === 'old') value = this.items[0]
+		if (value === 'old') value = item
 		else value = parseInt(value)
-
-		//console.log(item, operator, value)
-
-		switch (operator) {
-			case '+':
-				item += value
-				break
-			case '*':
-				item *= value
-				break
-		}
+		operator === '+' ? (item += value) : (item *= value)
 		this.inspectedItems++
 		this.items[0] = item
-		//console.log(item, operator, value)
 	}
-
 	testing() {
-		console.log('Execute Testing')
-		this.items[0] = Math.floor(this.items[0] / 3)
-		if (this.items[0] % this.test === 0) {
-			return this.monkeyIfTrue
+		if (p2) {
+			this.items[0] = this.items[0] % superModulo
 		} else {
-			return this.monkeyIfFalse
+			this.items[0] = Math.floor(this.items[0] / 3)
 		}
-	}
 
+		if (this.items[0] % this.test === 0) return this.monkeyIfTrue
+		else return this.monkeyIfFalse
+	}
 	throwItem() {
 		let item = this.items[0]
 		this.items.shift()
 		return item
 	}
-
 	addItem(item) {
 		this.items.push(item)
 	}
-
 	hasItems() {
-		if (this.items.length > 0) return true
-		else return false
+		return this.items.length > 0 ? true : false
 	}
 }
 
-for (let text of monkeyTexts) {
-	monkeys.push(new Monkey(text))
+// Parse Input
+let monkeyDNA = sliceIntoChunks(input, 7).map((chunk) => {
+	return chunk.slice(0, 6).map((line) => {
+		return line.replace(/\s/g, '')
+	})
+})
+// init a Array of Monkeys
+for (let dna of monkeyDNA) {
+	superModulo *= dna[3].slice(16)
+	monkeys.push(new Monkey(dna))
 }
-
+// Run the Simulation
 for (let i = 0; i < rounds; i++) {
 	for (let monkey of monkeys) {
 		while (monkey.hasItems()) {
-			//console.log('While Loop Monkey: ' + monkey.id)
-
-			//console.log(monkey)
 			monkey.inspect()
-			//console.log(monkey)
 			let receiver = monkey.testing()
-			//console.log(monkey)
 			monkeys[receiver].addItem(monkey.throwItem())
-			//console.log(monkey)
-			//console.log(' ')
 		}
 	}
 }
+// Find the Monkey with the most items inspected and calculate the result
+let monkeyBusiness = 1
+let sortedMonkeys = monkeys.sort((a, b) => b.inspectedItems - a.inspectedItems)
+sortedMonkeys.slice(0, 2).forEach((monkey) => {
+	monkeyBusiness *= monkey.inspectedItems
+})
 
-console.log(monkeys)
+console.log(monkeyBusiness)
